428db38f2640ed09c780c60ea45902b29f47daed
Yassine YBL <yassine.blila.95@gmail.com>
2024-10-20 13:33:19 +0100

    Update MySqlServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,7 +1,18 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-
+    host ="localhost",
+    user="Bl-Yassine",
+    password="Alx@SQL123",
+    database="alx_book_store"
 )
+
+cursor = mydb.cursor()
+
+cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+cursor.execute("USE alx_book_store")
+
+cursor.close()
+mydb.close()
 
 print("Database 'alx_book_store' created successfully!")
