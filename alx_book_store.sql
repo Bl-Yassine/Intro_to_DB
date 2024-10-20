8ef7c83fecc2595976f58647080c52230b8cf23b
Yassine YBL <yassine.blila.95@gmail.com>
2024-10-20 13:03:46 +0100

    Add alx_book_store.sql

---
+++ alx_book_store.sql
@@ -0,0 +1,37 @@
+CREATE TABLE Books(
+	Book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE,
+    publivation_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+CREATE TABLE Authors(
+	author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers(
+customer_id INT PRIMARY KEY,
+customer_name VARCHAR(215) NOT NULL,
+email VARCHAR(215) NOT NULL,
+address TEXT NOT NULL
+);
+
+CREATE TABLE Orders (
+order_id INT PRIMARY KEY,
+customer_id INT,
+order_date DATE,
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Order_Details (
+orderdetailid INT PRIMARY KEY,
+order_id INT ,
+book_id INT,
+quantity DOUBLE,
+FOREIGN KEY (book_id) REFERENCES Books(book_id),
+FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+);
+
